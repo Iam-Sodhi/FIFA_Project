df3dc6538f545f7502bc962833fb1fbbff4148d9
Gautam Sodhi <129111379+Iam-Sodhi@users.noreply.github.com>
2024-11-18 09:32:04 +0530

    #. Team page done

--- scriptTeams.js
+++ scriptTeams.js
@@ -59,16 +59,16 @@ function openTeamModal(team) {
                 <div class="modal-content">
                     <div class="modal-header">
                         <h5 class="modal-title" id="modalLabel-${team.id}">${team.name} Players</h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div class="modal-body">
                         <ul class="list-group">
                             ${team.players.map(player => `<li class="list-group-item">${player}</li>`).join('')}
                         </ul>
                     </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    </div>
+                       <div class="modal-footer">
+                     <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                      </div>
                 </div>
             </div>
         `;
